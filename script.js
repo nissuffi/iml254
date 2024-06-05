4e5cf4ebf579b1b5077bad97b0cae203f64de02e
nissuffi <152355542+nissuffi@users.noreply.github.com>
2024-06-05 23:34:29 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+
+<script>
+	/* When the user clicks on the button, 
+	toggle between hiding and showing the dropdown content */
+	function myFunction() {
+	  document.getElementById("myDropdown").classList.toggle("show");
+	}
+
+	// Close the dropdown if the user clicks outside of it
+	window.onclick = function(event) {
+	  if (!event.target.matches('.dropbtn')) {
+	    var dropdowns = document.getElementsByClassName("dropdown-content");
+	    var i;
+	    for (i = 0; i < dropdowns.length; i++) {
+	      var openDropdown = dropdowns[i];
+	      if (openDropdown.classList.contains('show')) {
+	        openDropdown.classList.remove('show');
+	      }
+	    }
+	  }
+	}
+</script>
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+let slideIndex = 0;
+let autoSlideInterval;
+
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";  
+    dots[slideIndex - 1].className += " active";
+
+    autoSlideInterval = setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+// Manual controls
+function plusSlides(n) {
+    clearTimeout(autoSlideInterval); // Clear the auto slide interval
+    slideIndex += n - 1;
+    showSlides();
+}
+
+function currentSlide(n) {
+    clearTimeout(autoSlideInterval); // Clear the auto slide interval
+    slideIndex = n - 1;
+    showSlides();
+}
+
+// Initial call to start the slideshow
+showSlides();
